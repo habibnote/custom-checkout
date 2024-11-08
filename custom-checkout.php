6dccb17fe7a9e8b0905d1c5260bf34df01eeef58
Habibur Rahman <habibblog97@gmail.com>
2024-11-08 11:22:06 +0600

    slick added

--- custom-checkout.php
+++ custom-checkout.php
@@ -21,6 +21,12 @@ function cc_habib_enqueue_scripts() {
     // Enqueue Google font
     wp_enqueue_script( 'google-font', '//fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap' );
 
+    if (is_checkout()) {
+        wp_enqueue_style( 'cc-habib-slick', plugin_dir_url(__FILE__) . 'assets/slick/slick.css', '', '0.0.1' );
+        wp_enqueue_style( 'cc-habib-slick-theme', plugin_dir_url(__FILE__) . 'assets/slick/slick-theme.css', '', '0.0.1' );
+        wp_enqueue_script( 'cc-habib-slick', plugin_dir_url(__FILE__) . 'assets/slick/slick.min.js', array('jquery'), time(), true );
+    }
+
     // Enqueue Custom CSS
     wp_enqueue_style( 'cc-habib-style', plugin_dir_url(__FILE__) . 'assets/css/style.css', '', time() );
 
